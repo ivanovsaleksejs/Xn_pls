cc056d5989d76b7890e556a08aa5d9f35fbbc54c
Raitis Stengrevics <dagrevis@gmail.com>
2014-10-12 16:00:56 +0300

    Remove some obvious abbrs

--- Bot/Abbr/Words.hs
+++ Bot/Abbr/Words.hs
@@ -6,9 +6,8 @@ abbr :: [(String, String)]
 abbr = 
     [
         ("afaik", "as far as I know"),
+        ("fyi", "for your information")
         ("imho", "in my humble opinion"),
         ("imo", "in my opinion"),
-        ("lol", "laughing out loud"),
-        ("omg", "oh my god"),
         ("til", "today I learned")
     ]
